2f357715b1d49c29211ddc89541a3f121f465899
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2024-04-28 14:17:29 +0900

    Create special.html

--- js/custom.js
+++ js/custom.js
@@ -69,4 +69,37 @@ $('.gnb a').click(function(e){
     fade: true,
     cssEase: 'linear'
   });
+
+  $('.subslider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 })
